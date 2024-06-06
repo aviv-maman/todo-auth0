c71278b09853cb39d99a5ef37efda23237fb96bc
Aviv Maman <awiwmaman@outlook.com>
2024-06-06 11:30:17 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className={inter.className}>
         <Providers>
           <Header />
-          <main className='md:mx-[10vw] min-h-[calc(100vh-154px)] sm:min-h-[calc(100vh-146px)] py-6'>{children}</main>
+          <main className='container md:mx-[10vw] min-h-[calc(100vh-154px)] sm:min-h-[calc(100vh-146px)] py-6'>
+            {children}
+          </main>
           <Footer />
           <Toaster />
         </Providers>
