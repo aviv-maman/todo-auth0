246d9bf0e1ee99d920eeef51eff748f1af718d38
Aviv Maman <awiwmaman@outlook.com>
2024-06-10 12:38:04 +0300

    Update warning-modal.tsx

--- components/warning-modal.tsx
+++ components/warning-modal.tsx
@@ -28,7 +28,7 @@ export const WarningModal: React.FC<WarningModalProps> = ({ isOpen, onClose, onC
       description='All associated data related with this post will be DELETED forever.'
       isOpen={isOpen}
       onClose={onClose}>
-      <div className='flex items-center justify-end w-full pt-6 space-x-2'>
+      <div className='flex items-center justify-end w-full space-x-2'>
         <Button disabled={loading} variant='outline' onClick={onClose}>
           Cancel
         </Button>
